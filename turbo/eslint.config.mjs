ff1cc3fe875fe5a97e52d7746e9f378a091b5d47
ccpu <m.doaie@hotmail.com>
2026-02-27 13:11:10 +0000

    chore(generators): introduce `scaffoldfy.json` for package generation
    * Added a new `scaffoldfy.json` configuration file for package generation.
    * Removed obsolete ESLint and TypeScript configuration files.
    * Updated package scripts to use `scaffoldfy` for package generation.

--- turbo/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import config from '../tooling/eslint/base.js';
-
-/** @type {import('typescript-eslint').Config} */
-export default config;
--- turbo/generators/config.ts
+++
@@ -1,9 +0,0 @@
-import { packageGenerator } from '@pixpilot/workspace-package-generator';
-
-module.exports = function generator(plop: unknown) {
-  packageGenerator(plop, {
-    author: 'm.doaie <m.doaie@hotmail.com>',
-    defaultBundler: 'tsdown',
-    orgName: 'pixpilot',
-  });
-};
